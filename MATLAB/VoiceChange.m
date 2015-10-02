8200d786424104858db70385e74e40174876b6fb
joshuadapitan <joshuadapitan@gmail.com>
2015-10-02 21:25:55 +0800

    Transposition values changed
    Adjusted values to adapt 8k sample .wav files

--- MATLAB/VoiceChange.m
+++ MATLAB/VoiceChange.m
@@ -1,10 +1,10 @@
 function VoiceChange(wavFile,gender)
     [data fs]=SpectralSub(wavFile);
     if gender==0
-        malSound=pitchShift(data,1024,256,-2);
+        malSound=pitchShift(data,round(fs/43.06640625),round((fs/43.06640625))/4,-2);
         sound(malSound,fs);
     else if gender==1
-        femSound=pitchShift(data,1024,256,2);
+        femSound=pitchShift(data,round(fs/43.06640625),round((fs/43.06640625)/4),2);
         sound(femSound,fs);
         else
             disp('You Didn''t pick an appropriate Gender');
